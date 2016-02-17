270fee14013c22483b6b82784b0d1e31ed25c547
Troels Lenda <troelslenda@gmail.com>
2016-02-17 16:37:19 +0100

    linking logo to frontpage

--- wp-content/themes/twentyfifteen-child/header.php
+++ wp-content/themes/twentyfifteen-child/header.php
@@ -28,8 +28,19 @@
   <div id="sidebar" class="sidebar">
     <header id="masthead" class="site-header" role="banner">
       <div class="site-branding">
-        <img src="/wp-content/themes/twentyfifteen-child/logo-header.png" />
-        
+        <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><img src="/wp-content/themes/twentyfifteen-child/logo-header.png" /></a>
+        <?php /*
+        if ( is_front_page() && is_home() ) : ?>
+          <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+        <?php else : ?>
+          <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+        <?php endif;
+
+        $description = get_bloginfo( 'description', 'display' );
+        if ( $description || is_customize_preview() ) : ?>
+          <p class="site-description"><?php echo $description; ?></p>
+        <?php endif; */
+        ?>
         <button class="secondary-toggle"><?php _e( 'Menu and widgets', 'twentyfifteen' ); ?></button>
       </div><!-- .site-branding -->
     </header><!-- .site-header -->
